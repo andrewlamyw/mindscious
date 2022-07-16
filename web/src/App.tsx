72cd838b7397e3de97af4661dbbf3031d1abcc3d
Andrew Lam <andrewlam.yw@gmail.com>
2022-07-16 10:26:01 +0800

    Init auth using clerk
    `yarn rw setup auth clerk`

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,6 @@
+import { ClerkProvider, withClerk } from '@clerk/clerk-react'
+
+import { AuthProvider } from '@redwoodjs/auth'
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
@@ -7,12 +10,35 @@ import Routes from 'src/Routes'
 import './scaffold.css'
 import './index.css'
 
+const ClerkAuthConsumer = withClerk(({ children, clerk }) => {
+  return React.cloneElement(children as React.ReactElement<any>, {
+    client: clerk,
+  })
+})
+
+const ClerkAuthProvider = ({ children }) => {
+  const frontendApi = process.env.CLERK_FRONTEND_API_URL
+  if (!frontendApi) {
+    throw new Error('Need to define env variable CLERK_FRONTEND_API_URL')
+  }
+
+  return (
+    <ClerkProvider frontendApi={frontendApi}>
+      <ClerkAuthConsumer>{children}</ClerkAuthConsumer>
+    </ClerkProvider>
+  )
+}
+
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <RedwoodApolloProvider>
-        <Routes />
-      </RedwoodApolloProvider>
+      <ClerkAuthProvider>
+        <AuthProvider type="clerk">
+          <RedwoodApolloProvider>
+            <Routes />
+          </RedwoodApolloProvider>
+        </AuthProvider>
+      </ClerkAuthProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
 )
