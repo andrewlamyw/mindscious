71f7b00401d2dac590f885c399dce70580e4bd5c
Andrew Lam <andrewlam.yw@gmail.com>
2022-07-16 16:03:41 +0800

    Add basic login/logout on HomePage

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,16 @@
-import { Redirect, routes } from '@redwoodjs/router'
+import { useAuth } from '@redwoodjs/auth'
 
-const HomePage = () => <Redirect to={routes.happinessRatings()} />
+const HomePage = () => {
+  const { currentUser, isAuthenticated, logIn, logOut } = useAuth()
+  return (
+    <>
+      <h1>👋Hi {isAuthenticated ? currentUser.firstName : 'there'}!</h1>
+
+      <button onClick={isAuthenticated ? logOut : logIn}>
+        {isAuthenticated ? 'Log Out' : 'Log In'}
+      </button>
+    </>
+  )
+}
 
 export default HomePage
